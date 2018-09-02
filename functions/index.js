42e57badec20c30d496eb4ee5c16c4d1c93bfe2f
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-09-02 16:02:19 +0530

    base EJS

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render('index');
+});
+app.get("/TheFACESeason", (req, res) => {
+    res.render('tfs');
+});
+app.get("/Blog", (req, res) => {
+    res.redirect('https://faceaseb.wordpress.com');
+});
+
+app.use((req, res, next) => {
+    res.status(404).render('404');
+});
+
+exports.app = functions.https.onRequest(app);
