66429157817aa354caf0eb2cd79705664e47d5ca
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-03-28 19:38:52 +0530

    SLAC route

--- functions/index.js
+++ functions/index.js
@@ -5,36 +5,41 @@ const app = express();
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    res.render('index');
+app.get('/', (req, res) => {
+	res.render('index');
 });
-app.get("/TFS", (req, res) => {
-    res.render('tfs');
+app.get('/TFS', (req, res) => {
+	res.render('tfs');
 });
-app.get("/Team", (req, res) => {
-    res.render('team');
+app.get('/Team', (req, res) => {
+	res.render('team');
 });
-app.get("/Offline", (req, res) => {
-    res.render('offline');
+app.get('/Offline', (req, res) => {
+	res.render('offline');
 });
-app.get("/Privacy", (req, res) => {
-    res.render('privacy');
+app.get('/Privacy', (req, res) => {
+	res.render('privacy');
 });
-app.get("/ICIC2018", (req, res) => {
-    res.render('icic2018');
+app.get('/ICIC2018', (req, res) => {
+	res.render('icic2018');
 });
-app.get("/Blog", (req, res) => {
-    res.redirect('https://faceaseb.wordpress.com');
+app.get('/Blog', (req, res) => {
+	res.redirect('https://faceaseb.wordpress.com');
 });
-app.get("/Identifire", (req, res) => {
-    res.redirect('https://drive.google.com/drive/folders/1_yxMFFhuUTfb-xTxqKeueHjkRiZC7Yyv');
+app.get('/Identifire', (req, res) => {
+	res.redirect(
+		'https://drive.google.com/drive/folders/1_yxMFFhuUTfb-xTxqKeueHjkRiZC7Yyv'
+	);
 });
-app.get("/UI-UX", (req, res) => {
-    res.render('uiux');
+app.get('/UI-UX', (req, res) => {
+	res.render('uiux');
+});
+app.get('/SLAC', (req, res) => {
+	res.redirect('https://slacfest.in');
 });
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+	res.status(404).render('404');
 });
 
 exports.app = functions.https.onRequest(app);
