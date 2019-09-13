5089ab87e2b75e40efe278139ab4c43fe66407b4
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-09-14 00:21:29 +0530

    Refactor

--- public/ACM/js/main.js
+++ public/ACM/js/main.js
@@ -1,21 +1,21 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    autoplay: 1000,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
-    }
-})
+	loop: true,
+	margin: 10,
+	nav: false,
+	autoplay: 1000,
+	responsive: {
+		0: {
+			items: 1
+		},
+		600: {
+			items: 2
+		},
+		1000: {
+			items: 3
+		}
+	}
+});
 
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
+$('.navbar-nav>li>a').on('click', function() {
+	$('.navbar-collapse').collapse('hide');
 });
