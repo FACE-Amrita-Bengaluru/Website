0eea6dc3338986cabff98c7b26bc03e28461f3bd
VinayakTekade <vinayak.tekade@gmail.com>
2019-09-27 21:17:56 +0530

    Added Scroll Indicator

--- public/ACM/js/main.js
+++ public/ACM/js/main.js
@@ -19,3 +19,11 @@ $('.owl-carousel').owlCarousel({
 $('.navbar-nav>li>a').on('click', function() {
 	$('.navbar-collapse').collapse('hide');
 });
+
+
+(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
