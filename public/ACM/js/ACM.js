d035079a21eca72ec1a542662da5739b3703a86c
Vinayak Tekade <39454332+VinayakTekade@users.noreply.github.com>
2020-03-26 18:27:09 +0530

    fix: ACM Bugs (#144)
    * fix: navbar logo
    
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
    
    * fix: ACM Members
    
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
    
    * fix: ACM home page screen responsive
    
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
    
    * fix: ACM logo center
    
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
    
    * feat: Button to SIG for small screen
    
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
    
    * fix: footer style on all pages
    
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
    
    * fix: ACM SIG screen responsiveness
    
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
    
    * fix: ACM Events screen responsiveness
    
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
    
    * fix: ACM Members screen responsiveness
    
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
    
    * feat: Removed Team jumbotron
    
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
    
    * refactor: ACM
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    Co-authored-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- public/ACM/js/ACM.js
+++ public/ACM/js/ACM.js
@@ -1,5 +1,4 @@
 //Owl Carousel
-
 $(".owl-carousel").owlCarousel({
 	loop: true,
 	margin: 10,
@@ -41,13 +40,10 @@ $(window).scroll(function() {
 function isScrolledIntoView(elem) {
 	var $elem = $(elem);
 	var $window = $(window);
-
 	var docViewTop = $window.scrollTop();
 	var docViewBottom = docViewTop + $window.height();
-
 	var elemTop = $elem.offset().top;
 	var elemBottom = elemTop + $elem.height();
-
 	return elemBottom <= docViewBottom && elemTop >= docViewTop;
 }
 
@@ -77,13 +73,19 @@ function checkScroll() {
 
 	if ($(window).scrollTop() > startY) {
 		$(".navbar").addClass("scrolled");
+		// $(".nav-logo").removeClass("logo");
+		// $(".nav-logo").addClass("logo-scrolled");
 	} else {
 		$(".navbar").removeClass("scrolled");
+		// $(".nav-logo").removeClass("logo-scrolled");
+		// $(".nav-logo").addClass("logo");
 	}
 }
 
-if ($(".navbar").length > 0) {
+if ($(".navbar").length > 0.7 || $(".navbar").length < 1.0) {
 	$(window).on("scroll load resize", function() {
 		checkScroll();
+		// changeLogo();
+		return;
 	});
 }
